24fbc54d5839b6d26e33a969c3f796afd95b693a
Carlton Joseph <carlton.joseph@gmail.com>
2020-07-13 15:23:38 -0700

    github oauth working

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'c185dabbb3b0f37b6538';
+const githubClientSecret = '0a2af28ae41831140bc24e315bbde5da662f134d';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
